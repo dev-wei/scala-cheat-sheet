e6201d3147b5caa4b591c682c2ac78688d6bb5a1
michael.wei <dev.wei@gmail.com>
2015-01-23 17:09:08 -0500

    check in

--- src/main/scala/HigherOrderFunctions.scala
+++ src/main/scala/HigherOrderFunctions.scala
@@ -1,16 +1,20 @@
 object HigherOrderFunctions extends App {
-  def sumOne(f: Int => Int): (Int, Int) => Int = {
-    def sumTwo(a: Int, b: Int): Int = {
-      return a
+  def sum(f: Int => Int): (Int, Int) => Int = {
+    def sumf(a: Int, b: Int): Int = {
+      return f(a + b)
     }
-    sumTwo
+    sumf
   }
 
-  def sumThree(f: Int => Int)(a: Int, b: Int): Int = {
-    return 1
-  }
+  def sumThree(f: Int => Int)(a: Int, b: Int): Int = f(a + b)
+
+  def cube(x: Int) = x * x * x
 
   override def main(args: Array[String]) {
-    println("Hello, world!")
+    println(sum(x => x * x * x)(1, 1))
+    println(sum(cube)(1, 1))
+
+    println(sumThree(x => x * x * x)(1, 1))
+    println(sumThree(cube)(1, 1))
   }
 }
